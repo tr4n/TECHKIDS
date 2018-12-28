8c1114f7e6e7616ea4603b39e0ec0ab5a1a872c5
tr4n <32348171+tr4n@users.noreply.github.com>
2018-12-28 17:13:24 +0700

    quicksort search elements

--- techkids-webfullstack18/lessons/lesson 1 - 27.12.18/html/main.js
+++ techkids-webfullstack18/lessons/lesson 1 - 27.12.18/html/main.js
@@ -1,18 +1,70 @@
-var array = [1, 5, 3, 4, 2, 3, 4];
-var length = array.length;
-
-
-console.log(array);
-for (let i = 0; i < length; i++) {
-    for (let j = i + 1; j < length; j++) {
-        if (array[i] > array[j]) {
-            var tmp = array[i];
-            array[i] = array[j];
-            array[j] = tmp;
-
-
-        }
-    }
+var Length = parseInt(20 * Math.random());
+var arr = [];
+for (var index = 0; index < Length; index++) {
+    arr[index] = parseInt(1000 * Math.random()) - 500;
 }
 
-console.log(array);
+
+function quickSort(array, first, second) {
+    var left = first,
+        right = second,
+        mid = parseInt((first + second) >> 1);
+    while (left < right) {
+        while (array[left] < array[mid]) left++;
+        while (array[right] > array[mid]) right--;
+
+        if (left <= right) {
+            if (left < right) {
+                var temp = array[left];
+                array[left] = array[right];
+                array[right] = temp;
+            }
+            left++;
+            right--;
+        }
+
+    }
+
+    if (first < right) quickSort(array, first, right);
+    if (left < second) quickSort(array, left, second);
+}
+
+function getSortedArray(array, first, second) {
+    var tempArray = array;
+    quickSort(tempArray, 0, array.length - 1);
+    return tempArray;
+
+}
+
+function getIndex(array, value) {
+    var position = -1;
+    for (var index = 0; index < array.length; index++) {
+        if (array[index] == value) {
+            position = index;
+            break;
+        }
+    }
+    return position;
+}
+
+var randomTest = parseInt(10 * Math.random());
+const INSIDE = 0,
+    OUTSIDE = 1;
+var mode = randomTest < 2 ? OUTSIDE : INSIDE;
+var number = 0,
+    index = 0;
+if (mode == OUTSIDE) {
+    number = 501 + parseInt(100 * Math.random());
+    index = -1;
+} else {
+    index = parseInt(Length * Math.random());
+    number = arr[index];
+}
+
+
+
+console.log(arr);
+console.log(getSortedArray(arr));
+console.log(number);
+console.log(index);
+console.log(getIndex(arr, number));
