ce5d3003826007c2005b308771cb5f67272bd566
tr4n <tranquanghuy.hsgs@gmail.com>
2019-01-13 17:10:31 +0700

    params

--- web-fullstack-18/lessons/lesson 5-03.01.19/quyetde/server.js
+++ web-fullstack-18/lessons/lesson 5-03.01.19/quyetde/server.js
@@ -2,22 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
 const app = express();
-
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-
-app.get("/", (request, response) => {
-    response.sendFile(__dirname + "/views/answer.html");
-});
-
-app.get("/ask", (request, response) => {
-    response.sendFile(__dirname + "/views/ask.html");
-});
-
-app.post("/add-question", (request, response) => {
-
-    const questionContent = request.body.questionContent;
+let Random = {
+    nextInt(value) {
+        return Math.floor(math.random() * value);
+    },
+    getInt(first, second) {
+        return first >= second ? second : first + Math.floor(Math.random() * (second - first))
+    }
+}
+let getQuestions = function () {
     let questions = [];
     if (!fs.existsSync(__dirname + "/views/database.json")) {
         fs.writeFileSync("views/database.json", JSON.stringify([]));
@@ -29,6 +22,59 @@ app.post("/add-question", (request, response) => {
         console.log(error);
     }
 
+    return questions;
+}
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.get("/", (request, response) => {
+    response.sendFile(__dirname + "/views/answer.html");
+    let questions = getQuestions(),
+        question;
+
+    if (questions.lenth == 0) {
+        response.send("Question list is empty");
+    } else {
+        const index = Math.floor(Math.random() * questions.length);
+        const randomQuestion = questions[index];
+        console.log(randomQuestion + "------------\n");
+        response.send(`
+        <h1>  ${randomQuestion.content}  <h1>
+        <a href="/vote/no/${randomQuestion.id}"><button> False </button></a>
+        <a href="/vote/yes/${randomQuestion.id}"><button> True </button></a>
+        `);
+    }
+
+});
+
+app.get("/vote/yes/:questionId", (request, response) => {
+    
+    const questions = getQuestions(),
+        { questionId } = (request.params),
+        question = questions.filter(item => item.id == questionId)[0];
+
+    console.log("Questions: --------------", questions);
+    questions.forEach((item, index) => {
+        if (item.id == questionId) {
+            questions[index].yes +=1;
+            console.log("item: ", item);
+        }
+    });
+    fs.writeFileSync("views/database.json", JSON.stringify(questions));
+    response.redirect("/");
+
+});
+
+app.get("/ask", (request, response) => {
+    response.sendFile(__dirname + "/views/ask.html");
+});
+
+app.post("/add-question", (request, response) => {
+
+    const questionContent = request.body.questionContent;
+    let questions = getQuestions();
     let newQuestion = {
         id: questions.length,
         content: questionContent,
@@ -38,9 +84,6 @@ app.post("/add-question", (request, response) => {
     questions.push(newQuestion);
     fs.writeFileSync("views/database.json", JSON.stringify(questions));
     response.redirect("/");
-
-
-
 });
 
 app.listen("1234", (error) => {
